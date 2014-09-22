238ba2f02df8f11f6985a36f4c490507c0f33ffd
mobmac <mobwi@outlook.com>
2014-09-22 10:07:29 +0800

    Remove in-app purchase

--- Agba/src/com/moba/gba/Agba.java
+++ Agba/src/com/moba/gba/Agba.java
@@ -24,7 +24,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,7 +31,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.androidemu.Emulator;
@@ -41,34 +39,22 @@ import com.androidemu.gba.input.GameKeyListener;
 import com.androidemu.gba.input.Keyboard;
 import com.androidemu.gba.input.Trackball;
 import com.androidemu.gba.input.VirtualKeypad;
+import com.gemuvn.gba.UpdateActivity;
 import com.moba.util.IabHelper;
 import com.moba.util.IabResult;
 import com.moba.util.Inventory;
 import com.moba.util.Purchase;
-import com.gemuvn.gba.PreferenceHelper;
-import com.moba.gba.R;
-import com.gemuvn.gba.UpdateActivity;
 import com.td.httpclient.HttpClientHelper;
 
 public class Agba extends Activity implements GameKeyListener,
 		DialogInterface.OnCancelListener {
 	// Debug tag, for logging
 	static final String TAG = "Agba";
-
-	// Does the user have the premium upgrade?
-	boolean mIsPremium = false;
-
-	// SKUs for our products: the premium upgrade (non-consumable)
-	static final String SKU_PREMIUM = "premium";
-
-	// (arbitrary) request code for the purchase flow
-	static final int RC_REQUEST = 10001;
-
-	// The helper object
-	IabHelper mHelper;
+	
+	static final boolean mIsPremium = false;
 
 	@SuppressWarnings("unused")
-	private static final String LOG_TAG = "GameBoid";
+	private static final String LOG_TAG = "Agba";
 
 	private static final int REQUEST_BROWSE_ROM = 1;
 	private static final int REQUEST_BROWSE_BIOS = 2;
@@ -92,16 +78,11 @@ public class Agba extends Activity implements GameKeyListener,
 	private Trackball trackball;
 	private View placeholder;
 
-	private Rect surfaceRegion = new Rect();
-	private int surfaceWidth;
-	private int surfaceHeight;
-
 	private String currentGame;
 	private String lastPickedGame;
 	private boolean isMenuShowing;
 	private int quickLoadKey;
 	private int quickSaveKey;
-	private FrameLayout adLeo = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -160,156 +141,7 @@ public class Agba extends Activity implements GameKeyListener,
 		}
 
 		showPlaceholder();
-		initIAB();
 	}
-
-	private void initIAB() {
-		/*
-		 * base64EncodedPublicKey should be YOUR APPLICATION'S PUBLIC KEY (that
-		 * you got from the Google Play developer console). This is not your
-		 * developer public key, it's the *app-specific* public key.
-		 * 
-		 * Instead of just storing the entire literal string here embedded in
-		 * the program, construct the key at runtime from pieces or use bit
-		 * manipulation (for example, XOR with some other string) to hide the
-		 * actual key. The key itself is not secret information, but we don't
-		 * want to make it easy for an attacker to replace the public key with
-		 * one of their own and then fake messages from the server.
-		 */
-		String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA17lRwYCLqj8LTFkOz7yUwsjfQ+zUIMpnOa5B8Z5OfOy6IPTRqoCXq7j6N9exNzGgyBDr3ENQKiMh6LEhtFe53CUQKx7Wzx4AtSLv8dM409/H/TEHxcFHGERdBw/JLv2kJLyRZXS8/LgzEXvK+zc8KtWSXpsa6Nhkv4N6Dsyy3Q7Q5JQ6C0Mne7JW30vLiMHYWlk5rUKqLyTYMoc2J9zp3V+Oo46A9jVjEnSUtqGoWAzkinakJUk2mPhb55ocvAQO5Gq2ybbndIGVTUFRSNwFPYgX0mXKZDMTnI3oWgzpNLvB/H3VTUg08QkKaRdDSOeBkNA7Z+hBaMtzNWEwBtJSxwIDAQAB";
-
-		Log.d(TAG, "Creating IAB helper.");
-		mHelper = new IabHelper(this, base64EncodedPublicKey);
-
-		// enable debug logging (for a production application, you should set
-		// this to false).
-		mHelper.enableDebugLogging(true);
-
-		// Start setup. This is asynchronous and the specified listener
-		// will be called once setup completes.
-		Log.d(TAG, "Starting setup.");
-		mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-			public void onIabSetupFinished(IabResult result) {
-				Log.d(TAG, "Setup finished.");
-
-				if (!result.isSuccess()) {
-					// Oh noes, there was a problem.
-					complain("Problem setting up in-app billing: " + result);
-					return;
-				}
-
-				// Have we been disposed of in the meantime? If so, quit.
-				if (mHelper == null)
-					return;
-
-				// IAB is fully set up. Now, let's get an inventory of stuff we
-				// own.
-				Log.d(TAG, "Setup successful. Querying inventory.");
-				mHelper.queryInventoryAsync(mGotInventoryListener);
-			}
-		});
-
-	}
-
-	// Listener that's called when we finish querying the items and
-	// subscriptions we own
-	IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-		public void onQueryInventoryFinished(IabResult result,
-				Inventory inventory) {
-			Log.d(TAG, "Query inventory finished.");
-
-			// Have we been disposed of in the meantime? If so, quit.
-			if (mHelper == null)
-				return;
-
-			// Is it a failure?
-			if (result.isFailure()) {
-				complain("Failed to query inventory: " + result);
-				return;
-			}
-
-			Log.d(TAG, "Query inventory was successful.");
-
-			/*
-			 * Check for items we own. Notice that for each purchase, we check
-			 * the developer payload to see if it's correct! See
-			 * verifyDeveloperPayload().
-			 */
-
-			// Do we have the premium upgrade?
-			Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
-			mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
-			Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
-
-			updateUi();
-			setWaitScreen(false);
-			Log.d(TAG, "Initial inventory query finished; enabling main UI.");
-		}
-	};
-
-	// updates UI to reflect model
-	public void updateUi() {
-		// "Upgrade" button is only visible if the user is not premium
-		findViewById(R.id.upgrade).setVisibility(
-				mIsPremium ? View.GONE : View.VISIBLE);
-	}
-
-	// Enables or disables the "please wait" screen.
-	void setWaitScreen(boolean set) {
-		findViewById(R.id.empty).setVisibility(set ? View.GONE : View.VISIBLE);
-		findViewById(R.id.screen_wait).setVisibility(
-				set ? View.VISIBLE : View.GONE);
-	}
-
-	void complain(String message) {
-		Log.e(TAG, "**** Agba Error: " + message);
-		alert("Error: " + message);
-	}
-
-	void alert(String message) {
-		AlertDialog.Builder bld = new AlertDialog.Builder(this);
-		bld.setMessage(message);
-		bld.setNeutralButton("OK", null);
-		Log.d(TAG, "Showing alert dialog: " + message);
-		bld.create().show();
-	}
-
-	/** Verifies the developer payload of a purchase. */
-	boolean verifyDeveloperPayload(Purchase p) {
-		String payload = p.getDeveloperPayload();
-
-		/*
-		 * TODO: verify that the developer payload of the purchase is correct.
-		 * It will be the same one that you sent when initiating the purchase.
-		 * 
-		 * WARNING: Locally generating a random string when starting a purchase
-		 * and verifying it here might seem like a good approach, but this will
-		 * fail in the case where the user purchases an item on one device and
-		 * then uses your app on a different device, because on the other device
-		 * you will not have access to the random string you originally
-		 * generated.
-		 * 
-		 * So a good developer payload has these characteristics:
-		 * 
-		 * 1. If two different users purchase an item, the payload is different
-		 * between them, so that one user's purchase can't be replayed to
-		 * another user.
-		 * 
-		 * 2. The payload must be such that you can verify it even when the app
-		 * wasn't the one who initiated the purchase flow (so that items
-		 * purchased by the user on one device work on other devices owned by
-		 * the user).
-		 * 
-		 * Using your own server to store and verify developer payloads across
-		 * app installations is recommended.
-		 */
-
-		if ("upgrademoba".equals(payload))
-			return true;
-		return false;
-	}
-
-	Handler mHandler = new Handler();
 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
@@ -335,13 +167,6 @@ public class Agba extends Activity implements GameKeyListener,
 			resumeRequested = 0;
 			emulator.cleanUp();
 			emulator = null;
-		}
-
-		// very important:
-		Log.d(TAG, "Destroying helper.");
-		if (mHelper != null) {
-			mHelper.dispose();
-			mHelper = null;
 		}
 	}
 
@@ -505,85 +330,37 @@ public class Agba extends Activity implements GameKeyListener,
 	public void onUpgradeAppButtonClicked(View arg0) {
 		Log.d(TAG,
 				"Upgrade button clicked; launching purchase flow for upgrade.");
-		setWaitScreen(true);
-
-		/*
-		 * TODO: for security, generate your payload here for verification. See
-		 * the comments on verifyDeveloperPayload() for more info. Since this is
-		 * a SAMPLE, we just use an empty string, but on a production app you
-		 * should carefully generate this.
-		 */
-		String payload = "upgrademoba";
-
-		mHelper.launchPurchaseFlow(this, SKU_PREMIUM, RC_REQUEST,
-				mPurchaseFinishedListener, payload);
+		
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri
+				.parse("https://play.google.com/store/apps/details?id=com.moba.gbapro"));
+		startActivity(i);
 	}
-
-	// Callback for when a purchase is finished
-	IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
-		public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-			Log.d(TAG, "Purchase finished: " + result + ", purchase: "
-					+ purchase);
-
-			// if we were disposed of in the meantime, quit.
-			if (mHelper == null)
-				return;
-
-			if (result.isFailure()) {
-				complain("Error purchasing: " + result);
-				setWaitScreen(false);
-				return;
-			}
-			if (!verifyDeveloperPayload(purchase)) {
-				complain("Error purchasing. Authenticity verification failed.");
-				setWaitScreen(false);
-				return;
-			}
-
-			Log.d(TAG, "Purchase successful.");
-
-			if (purchase.getSku().equals(SKU_PREMIUM)) {
-				// bought the premium upgrade!
-				Log.d(TAG, "Purchase is premium upgrade. Congratulating user.");
-				alert("Thank you for upgrading to premium!");
-				mIsPremium = true;
-				updateUi();
-				setWaitScreen(false);
-			}
-		}
-	};
 
 	@Override
 	protected void onActivityResult(int request, int result, Intent data) {
 		Log.d(TAG, "onActivityResult(" + request + "," + result + "," + data);
-		if (mHelper == null)
-			return;
-
-		// Pass on the activity result to the helper for handling
-		if (!mHelper.handleActivityResult(request, result, data)) {
-			// not handled, so handle it ourselves (here's where you'd
-			// perform any handling of activity results not related to in-app
-			// billing...
-			switch (request) {
-			case REQUEST_BROWSE_ROM:
-				if (result == RESULT_OK) {
-					lastPickedGame = data
-							.getStringExtra(FileChooser.EXTRA_FILEPATH);
-					loadROM(lastPickedGame);
-				}
-				break;
-
-			case REQUEST_BROWSE_BIOS:
-				loadBIOS(result == RESULT_OK ? data
-						.getStringExtra(FileChooser.EXTRA_FILEPATH) : null);
-				break;
-
-			case REQUEST_SETTINGS:
-				loadGlobalSettings();
-				break;
+		
+		// not handled, so handle it ourselves (here's where you'd
+		// perform any handling of activity results not related to in-app
+		// billing...
+		switch (request) {
+		case REQUEST_BROWSE_ROM:
+			if (result == RESULT_OK) {
+				lastPickedGame = data
+						.getStringExtra(FileChooser.EXTRA_FILEPATH);
+				loadROM(lastPickedGame);
 			}
-		} else {
-			Log.d(TAG, "onActivityResult handled by IABUtil.");
+			break;
+
+		case REQUEST_BROWSE_BIOS:
+			loadBIOS(result == RESULT_OK ? data
+					.getStringExtra(FileChooser.EXTRA_FILEPATH) : null);
+			break;
+
+		case REQUEST_SETTINGS:
+			loadGlobalSettings();
+			break;
 		}
 
 	}
